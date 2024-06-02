c35c00246434b2456d29fd6cd51dc9b43955accf
Luca D'Angelo <ldgaetano@protonmail.com>
2024-06-01 23:55:24 -0400

    Add second signer logic.

--- src/app/instance2_sign.ts
+++ src/app/instance2_sign.ts
@@ -6,6 +6,13 @@ import { ErgoContext } from '../rust/ErgoContext';
 import { BackendWallet } from '../rust/BackendWallet';
 import { hex } from '@fleet-sdk/crypto';
 import { sign } from '../multisig/multisign';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+// SUPABASE: Initialize supabase client.
+const supabase_url = process.env.SUPABASE_LINK as string
+const supabase_service_key = process.env.SUPABASE_SERVICE_KEY as string
+console.log(supabase_url, supabase_service_key)
+const supabase: SupabaseClient = createClient(supabase_url, supabase_service_key)
 
 const mnemonic2 =
   'swim gym asset pipe improve dismiss eagle federal december play habit culture innocent sleep exist';
@@ -27,28 +34,57 @@ const node = new NodeAPI(nodeUrl);
 
 const wallet = new BackendWallet(mnemonic2, mnemonicPw, Network.Testnet); // PC
 
-const commitmentResult = {
-  commitments: [
-    [
-      'ArLgf4HY611QJfgIcK6ltBsKpvlSpgr12yUEV8q5t2MA',
-      'A7/TPo/Ebg3rfpghpRYZPDPiltWPKXQmuHXW5cLv71Xv',
-    ],
-  ],
-  secrets: [
-    [
-      '',
-      'U2FsdGVkX1885KTby1coTxJ3d6mqQeZz9CtvKwWUyycOenv1kevoTtWUyY3Z1Fu5zhh+1a3+YKTjTHdEqKskboR7V2jITsVQPKGI1A3qrm21PFtc0GWoXR4QLXS59V6v',
-    ],
-  ],
-  signed: [],
-  simulated: [],
-};
+const user_address = "3WzNDoTs6sJBaBHXwMAXEGz8gFrHMcy6Y4GszD1evZgELZ6QvbRc"
+const channel_user = await supabase
+  .from('msig_channel_msig_user')
+  .select('channel')
+  .eq('member', user_address)
+
+if (channel_user.error) throw channel_user.error
+console.log("channels user belongs to: ", channel_user.data)
+
+// select a channel
+const channel_id = channel_user.data.pop()?.channel
+
+// get wallet from channel
+const msig_wallet = await supabase
+  .from('msig_wallet')
+  .select()
+  .eq('channel', channel_id)
+
+if (msig_wallet.error) throw msig_wallet.error
+console.log("wallet from the channel the user belongs to: ", msig_wallet.data)
+
+// get unsigned transactions from wallet, if any exist
+const unsigned_txs = await supabase
+  .from('msig_unsigned_tx')
+  .select()
+  .eq('msig_wallet', msig_wallet.data[0].id)
+
+if (unsigned_txs.error) throw unsigned_txs.error
+console.log("unsigned transaction in the msig wallet: ", unsigned_txs.data)
+
+// select an unsigned tx
+const unsigned_tx = unsigned_txs.data.pop()
+
+// get user secrets from unsigned tx
+const user_secrets = await supabase
+  .from('msig_user_commit_msig_unsigned_tx')
+  .select('commit_secrets')
+  .eq('msig_unsigned_tx', unsigned_tx.id)
+  .eq('msig_user', user_address)
+
+if (user_secrets.error) throw user_secrets.error
+console.log("the user secrets: ", user_secrets.data)
+
+let data = unsigned_tx.commit_data
+data.secrets = user_secrets.data[0].commit_secrets
+console.log("commit with secrets: ", data)
 
 const customWallet = {
   addresses: [
     {
-      address:
-        'gG26HKJqFfxo7wEyBmjX7BYZURh3pojtMdxhuf3jcVR5QdUgR2rxLMxQqDnVCL77Q3YL5KncqftcD8JaAjcdQ45tRgZDQruFF9aihKaeT3bnFAVWSnvog5c58',
+      address: msig_wallet.data[0].msig_address
     },
   ],
 };
@@ -62,79 +98,90 @@ const signer2 = {
   ],
 };
 
-const commited = [
-  {
-    address: '3WwuMNsdLxX1PetQAggX2mnbfJW4CDXuyScqak8UHwv1MSLKRZKr',
-    completed: true,
-  },
-  {
-    address: '3WzNDoTs6sJBaBHXwMAXEGz8gFrHMcy6Y4GszD1evZgELZ6QvbRc',
-    completed: true,
-  },
-];
+const getCommitted = async (unsigned_tx: any, msig_wallet: any) => {
 
-const addresses = [
-  {
-    address: '3WwuMNsdLxX1PetQAggX2mnbfJW4CDXuyScqak8UHwv1MSLKRZKr',
-    xPub: 'xpub6FQV2qbwPeW24osq6AyejYhpmyWkTuewoienVJDFC2u9EBv4doucz6eK6w7a21A6wmWk8qPFn3WjYPfcTBVNNFJdkm13y3J1uJyEDypa9Bb',
-    pubKeys: [
-      '02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf04',
-    ],
-  },
-  {
-    address: '3WzNDoTs6sJBaBHXwMAXEGz8gFrHMcy6Y4GszD1evZgELZ6QvbRc',
-    xPub: 'xpub6E1HSNjA3Ji7EJqErXJ7uPmK6irB9ma3kCrRnpgqqFM3fJQ88HYe86722cEnJQ6ZqQbY6y8F6wX4pWes1yzYGSVgpMVxWwRNzeYeByotFZ3',
-    pubKeys: [
-      '03ef05f4324f39ddeb57e21c88e5c9d3ed2653af30aeec65b70f836ce259a69a20',
-    ],
-  },
-];
+  const signers = msig_wallet.signers
+  const committed = await supabase
+    .from('msig_user_commit_msig_unsigned_tx')
+    .select('msig_user')
+    .eq('msig_unsigned_tx', unsigned_tx.id)
+  if (committed.error) throw committed.error
+  console.log(committed.data)
 
-const reducedTxHexString =
-  'a402016c7fe9866aa0d9abe3bf774878b32d6235c686650fff54c4fb3cdfe4d86e2d7d0000000003a082a8dc030008cd02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf04a3a7480000e091431005040004000e36100204a00b08cd0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ea02d192a39a8cc7a701730073011001020402d19683030193a38cc7b2a57300000193c2b2a57301007473027303830108cdeeac93b1a57304a3a7480000e0c2f4e9851d1003040408cd02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf0408cd03ef05f4324f39ddeb57e21c88e5c9d3ed2653af30aeec65b70f836ce259a69a2098730083020873017302a3a74800009602cd02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf04cd03ef05f4324f39ddeb57e21c88e5c9d3ed2653af30aeec65b70f836ce259a69a200000';
+  return signers.map((signer: any) => {
+
+    const has_commit = committed.data.includes(signer.address)
+
+    return {
+      address: signer.address,
+      completed: has_commit
+    }
+
+  })
+
+}
+
+const getSigned = async (unsigned_tx: any, msig_wallet: any) => {
+
+  const signers = msig_wallet.signers
+  const signed = await supabase
+    .from('msig_user_sign_msig_unsigned_tx')
+    .select('msig_user')
+    .eq('msig_unsigned_tx', unsigned_tx.id)
+  if (signed.error) throw signed.error
+  console.log(signed.data)
+
+  return signers.map((signer: any) => {
+
+    const has_signature = signed.data.includes(signer.address)
+
+    return {
+      address: signer.address,
+      completed: has_signature
+    }
+
+  })
+
+}
+
+const committed = await getCommitted(unsigned_tx, msig_wallet.data[0])
+
+const signed2 = await getSigned(unsigned_tx, msig_wallet.data[0])
+
+const addresses = msig_wallet.data[0].signers
+
+const reducedTxHexString = unsigned_tx.reduced_tx_hex
 
 const reducedTransaction = ReducedTransaction.sigma_parse_bytes(
   hex.decode(reducedTxHexString),
 );
 
-const inputIds = reducedTransaction
-  .unsigned_tx()
-  .to_js_eip12()
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  .inputs.map((input) => input.boxId);
 
-const inputs = await node.getUtxos(inputIds);
+const inputs = unsigned_tx.unsigned_tx.inputs
 
-const wasmInputs = inputs!.map((input) =>
+const wasmInputs = inputs!.map((input: any) =>
   ErgoBox.from_json(JSON.stringify(input)),
 );
 
 const ergoContext = new ErgoContext(node);
 const ctx = await ergoContext.getStateContext();
 
-const signedData = {
-  signed: ['3WwuMNsdLxX1PetQAggX2mnbfJW4CDXuyScqak8UHwv1MSLKRZKr'],
-  simulated: [],
-  partial:
-    '016c7fe9866aa0d9abe3bf774878b32d6235c686650fff54c4fb3cdfe4d86e2d7d587cb4e241b0e58b39621d73075f945e9200b234eeed9373e0c912a74f5208bae29f7f4c31a32100005f46566629351d53d18871476be77ab3958e980ebc423a32a8682a58c3af8a3868c4d6791bb4f0fe4119a4f8c3188c7300000003a082a8dc030008cd02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf04a3a7480000e091431005040004000e36100204a00b08cd0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ea02d192a39a8cc7a701730073011001020402d19683030193a38cc7b2a57300000193c2b2a57301007473027303830108cdeeac93b1a57304a3a7480000e0c2f4e9851d1003040408cd02aa9ef05d5a178d53bd34be0730c958f82155307e7e2e2a436f60a5414dbcaf0408cd03ef05f4324f39ddeb57e21c88e5c9d3ed2653af30aeec65b70f836ce259a69a2098730083020873017302a3a7480000',
-  currentTime: 1717256786789,
-};
-
-const signed2 = addresses.map((address) => {
-  return {
-    address: address.address,
-    completed: signedData.signed.includes(address.address),
-  };
-});
+const getOldPartial = (unsigned_tx: any) => {
+  const signatureData = unsigned_tx.signature_data
+  if (signatureData) {
+    return Transaction.sigma_parse_bytes(hex.decode(signatureData.partial))
+  } else {
+    return undefined
+  }
+}
 
 const signResult = await sign(
   customWallet,
   signer2,
-  commitmentResult.simulated, // []
-  commitmentResult.commitments, // both commitments are present
-  commitmentResult.secrets, // this has to be PC secret
-  commited,
+  data.simulated, // []
+  data.commitments, // both commitments are present
+  data.secrets, // this has to be PC secret
+  committed,
   signed2,
   addresses,
   reducedTransaction,
@@ -142,11 +189,53 @@ const signResult = await sign(
   'mypw',
   ctx,
   wallet.getWallet(0),
-  Transaction.sigma_parse_bytes(hex.decode(signedData.partial)),
+  getOldPartial(unsigned_tx),
 );
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 signResult.partial = hex.encode(signResult.partial.sigma_serialize_bytes());
-
 console.log(JSON.stringify(signResult));
+
+const getSignStatus = (signature: any, wallet: any) => {
+
+  const threshold = wallet.msig_threshold
+  const size = wallet.msig_size
+  const signed = signature.signed.length
+  
+  if (threshold == signed) {
+    return "THRESHOLD_REACHED"
+  } else if (threshold > signed) {
+    return "THRESHOLD_EXCEEDED"
+  } else if (size == signed) {
+    return "RING_COMPLETE"
+  } else {
+    return "STARTED"
+  }
+
+}
+
+const update_unsigned_tx = await supabase 
+  .from('msig_unsigned_tx')
+  .update({
+    sign_status: getSignStatus(signResult, msig_wallet.data[0]),
+    signature_data: signResult
+  })
+  .eq('id', unsigned_tx.id)
+  .select()
+
+if (update_unsigned_tx.error) throw update_unsigned_tx.error
+console.log("updated unsigned_tx data: ", update_unsigned_tx.data)
+
+// SUPABASE: Store user secrets
+const user_signature = await supabase
+  .from('msig_user_sign_msig_unsigned_tx')
+  .insert({
+    msig_user: user_address,
+    msig_unsigned_tx: unsigned_tx.id,
+    user_signature_data: signResult
+  })
+  .select()
+
+if (user_signature.error) throw user_signature.error
+console.log("user secrets: ", user_signature.data)
